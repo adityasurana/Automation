832dfe973cbe88133610677af34b1e722243dc74
Aditya Surana <to.adityasurana@gmail.com>
2023-01-04 19:06:01 +0530

    Update linkedin.py

--- linkedin.py
+++ linkedin.py
@@ -1,12 +1,9 @@
 from linkedin_api import Linkedin
 
 # Authenticate using any Linkedin account credentials
-api = Linkedin('to.adityasurana@gmail.com', 'CHIMMUSURANA1@')
+api = Linkedin('to.adityasurana@gmail.com', '*****')
 url = "https://www.linkedin.com/in/stephanie-caldwell-79b62526/"
-result = api.search_people(keywords="stephanie-caldwell-79b62526")
-print(result)
 
-#result = api.get_profile_connections("stephanie-caldwell-79b62526")
+## Getting people detail
 newResult = api.get_profile("stephanie-caldwell-79b62526")
-print("hi")
 print(newResult)
